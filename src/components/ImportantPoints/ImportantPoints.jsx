6284b1d508f0df5d66ff93d714e72927f802097a
katmoraesx <kathelyn_andrade23@icloud.com>
2025-04-15 11:25:19 -0300

    First commit.

--- src/components/ImportantPoints/ImportantPoints.jsx
+++ src/components/ImportantPoints/ImportantPoints.jsx
@@ -3,9 +3,18 @@ export default function ImportantPoints() {
     <div className="bg-gray-100 p-5 shadow-lg w-1/4 text-sm rounded-md">
       <h2 className="font-bold text-center mb-2">PONTOS IMPORTANTES</h2>
       <ul className="space-y-2 text-gray-700">
-        <li><span className="inline-block w-4 h-4 bg-red-600 rounded-full mr-2"></span>Ponto de Encontro</li>
-        <li><span className="inline-block w-4 h-1 bg-black mr-2"></span>Rotas Principais</li>
-        <li><span className="inline-block w-4 h-4 bg-blue-600 rounded-full mr-2"></span>Ponto de Referência</li>
+        <li>
+          <span className="inline-block w-4 h-4 bg-red-600 rounded-full mr-2"></span>
+          Ponto de Encontro
+        </li>
+        <li>
+          <span className="inline-block w-4 h-1 bg-black mr-2"></span>Rotas
+          Principais
+        </li>
+        <li>
+          <span className="inline-block w-4 h-4 bg-blue-600 rounded-full mr-2"></span>
+          Ponto de Referência
+        </li>
       </ul>
     </div>
   );
